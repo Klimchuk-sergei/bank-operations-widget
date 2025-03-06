c9af3a70cd19c9300633c1cef2c06c976e54158b
Klimchuk_Sergei <klim.work@mail.ru>
2025-03-06 20:43:34 +0300

    main добавлен вызов функции

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card, get_date
 
+# Пример вызова функций из masks.py
 print(get_mask_account("72777747288831274747"))
 print(get_mask_card_number("2598415763546857"))
+
+# Пример вызова функций из widget.py
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(get_date("2024-03-11T02:26:18.671407"))
