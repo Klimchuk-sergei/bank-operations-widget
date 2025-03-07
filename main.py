7b05419beca13bfbed60e619d6e3612ed7f0e794
Klimchuk_Sergei <klim.work@mail.ru>
2025-03-07 17:24:43 +0300

    upd main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 # Пример вызова функций из masks.py
 print(get_mask_account("72777747288831274747"))
--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,20 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(data: str) -> str:
     """
     Скрываем номер карты или счета.
     """
-    parts = data.split() #Разделяем входные данные на части
+    parts = data.split()    # Разделяем входные данные на части
     if len(parts) < 2:
         raise ValueError("Некорректный формат входных данных.")
 
-    account_type = " ".join(parts[:-1]) #Получаем название карты или счета
-    number = parts[-1] #Берем номер
+    account_type = " ".join(parts[:-1])     # Получаем название карты или счета
+    number = parts[-1]      # Берем номер
 
-    if number.isdigit() and len(number) == 16: #Проверка на карту
+    if number.isdigit() and len(number) == 16:      # Проверка на карту
         masked_number = get_mask_card_number(number)
-    elif number.isdigit() and len(number) > 4: #Проверка на счет
+    elif number.isdigit() and len(number) > 4:      # Проверка на счет
         masked_number = get_mask_account(number)
     else:
         raise ValueError("Некорректный номер карты или счёта.")
@@ -26,7 +26,7 @@ def get_date(date_str: str) -> str:
     """
     Преобразует дату из формата '2024-03-11T02:26:18.671407' в 'ДД.ММ.ГГГГ'.
     """
-    date_part = date_str.split("T")[0] #Разделяем по букве Т
+    date_part = date_str.split("T")[0]      # Разделяем по букве Т
     year, month, day = date_part.split("-")
     formatted_date = f"{day}.{month}.{year}"
     return formatted_date
