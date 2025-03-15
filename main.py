67dff56601c253deadccf53c2e5b4d913d228134
Klimchuk_Sergei <klim.work@mail.ru>
2025-03-15 16:27:59 +0300

    Отсортировал импорты в main.py с помощью isort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import get_date, mask_account_card
 from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
 
 # Пример вызова функций из masks.py
 print(get_mask_account("72777747288831274747"))
@@ -12,10 +12,10 @@ print(get_date("2024-03-11T02:26:18.671407"))
 
 # Фильтрация и сортировка операций
 operations = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
 filtered_operations = filter_by_state(operations, "EXECUTED")
